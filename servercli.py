aeb18a4cb184a5fd7cb73f3e5b8b74dbcaa55690
randolph182 <randolph.estuardo.2012@gmail.com>
2020-06-27 17:33:25 -0600

    Update servercli.py

--- servercli.py
+++ servercli.py
@@ -1,6 +1,7 @@
 import asyncio
 import json
 import pymongo
+import redis
 from nats.aio.client import Client as NATS
 from nats.aio.errors import ErrConnectionClosed, ErrTimeout, ErrNoServers
 
@@ -14,15 +15,23 @@ async def run(loop):
         con=pymongo.MongoClient('34.70.196.45',27017)
         try:
             db=con.proyecto2
-            db.casos.insert({"name":data["name"],"depto":data["depto"],"age":10,"form":data["form"],"state":data["state"]})
+            db.casos.insert({"name":data["name"],"depto":data["depto"],"age":data["age"],"form":data["form"],"state":data["state"]})
             print("datos enviados a mongo")
         except Exception as e:
             print(e)
             print("problemas con la conexion de mongo")
         finally:
             con.close()
+        #print(data)
+        # ======== enviando datos a redis ================
+        try:
+            r = redis.Redis(host='34.70.196.45',port=6379)
+            r.rpush('proyecto2','{"name" : "'+data["name"]+'", "depto" : "'+data["depto"]+'", "age" :'+str(data["age"])+', "form" : "'+data["form"]+'", "state" : "'+data["state"]+'"}')
+            print("datos enviados a redis")
+        except Exception as e:
+            print(e)
+            print("hay problemas en la conexion con redis")
         print(data)
-
     await nc.subscribe("updates", cb=message_handler)
 
 if __name__ == '__main__':
